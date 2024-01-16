676c24273148f043838dd50aa53a1278e50a06ec
mohammed amine Kabbour <Mohammedamine.Kabbour@emsi-edu.ma>
2024-01-16 11:25:41 +0100

    first commit

---
+++ src/main/java/com/xproce/repositories/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.xproce.repositories;
+
+import com.xproce.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    List<Customer> findByEmail(String email);
+}
